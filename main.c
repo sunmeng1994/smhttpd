513b39bb140c4c248694b2e05e3974497db34f1a
孙猛 <meng.sun@k7.cn>
2019-05-13 17:29:31 +0800

    检查是否是accept_request的问题

--- main.c
+++ main.c
@@ -180,80 +180,84 @@ int get_line(int sock,char*buf,int size)
 }
 
 //接收客户端的连接
-void accept_request(void*from_client)
+void accept_request(void *arg)
 {
-    int client=(intptr_t)from_client;
+    int client = (intptr_t)arg;
     char buf[1024];
-    int numchars;
+    size_t numchars;
     char method[255];
     char url[255];
     char path[512];
-    size_t i,j;
+    size_t i, j;
     struct stat st;
-    int cgi=0;
-    char*query_string=NULL;
-    numchars=get_line(client,buf,sizeof(buf));
-    i=0;
-    j=0;
-    while(!ISspace(buf[i])&&(i<sizeof(method)-1))
+    int cgi = 0;      /* becomes true if server decides this is a CGI
+                       * program */
+    char *query_string = NULL;
+
+    numchars = get_line(client, buf, sizeof(buf));
+    i = 0; j = 0;
+    while (!ISspace(buf[i]) && (i < sizeof(method) - 1))
     {
-	method[i]=buf[i];
-	i++;
+        method[i] = buf[i];
+        i++;
     }
     j=i;
-    method[i]='\0';
-    //strcasecmp 忽略大小写比较，相同返回0
-    if(strcasecmp(method,"GET")&&strcasecmp(method,"POST"))
+    method[i] = '\0';
+
+    if (strcasecmp(method, "GET") && strcasecmp(method, "POST"))
     {
-	unimplemented(client);
-	return NULL;
-    }
-    if(strcasecmp(method,"post")==0)
-	cgi=1;
-    i=0;
-    while(ISspace(buf[j])&&(j<sizeof(buf)))
-	j++;
-    while(!ISspace(buf[j])&&i<sizeof(url)-1&&j<sizeof(buf))
-    {
-	url[i]=buf[j];
-	i++;j++;
-    }
-    url[i]='\0';	
-    if(strcasecmp(method,"GET")==0)
-    {
-	query_string=url;
-	while((*query_string!='?')&&(*query_string!='\0'))
-	    query_string++;
-	if(*query_string=='?')
-	{
-	    cgi=1;
-	    *query_string='\0';
-	    query_string++;
-	}
+        unimplemented(client);
+        return;
     }
 
-    sprintf(path,"htdocs%s",url);
-    if(path[strlen(path)-1]=='/')
-	strcat(path,"index.html");
-    if(stat(path,&st)==-1)
+    if (strcasecmp(method, "POST") == 0)
+        cgi = 1;
+
+    i = 0;
+    while (ISspace(buf[j]) && (j < numchars))
+        j++;
+    while (!ISspace(buf[j]) && (i < sizeof(url) - 1) && (j < numchars))
     {
-	while(numchars>0&&strcmp("\n",buf))
-	    numchars=get_line(client,buf,sizeof(buf));
-	not_found(client);
+        url[i] = buf[j];
+        i++; j++;
+    }
+    url[i] = '\0';
+
+    if (strcasecmp(method, "GET") == 0)
+    {
+        query_string = url;
+        while ((*query_string != '?') && (*query_string != '\0'))
+            query_string++;
+        if (*query_string == '?')
+        {
+            cgi = 1;
+            *query_string = '\0';
+            query_string++;
+        }
+    }
+
+    sprintf(path, "htdocs%s", url);
+    if (path[strlen(path) - 1] == '/')
+        strcat(path, "index.html");
+    if (stat(path, &st) == -1) {
+        while ((numchars > 0) && strcmp("\n", buf))  /* read & discard headers */
+            numchars = get_line(client, buf, sizeof(buf));
+        not_found(client);
     }
     else
     {
-	if((st.st_mode&S_IFMT)==S_IFDIR)
-	    strcat(path,"/index.html");
-	if((st.st_mode&S_IXUSR)||
-	    (st.st_mode&S_IXGRP)||
-	    (st.st_mode&S_IXOTH))
-	    cgi=1;
-	if(!cgi)
-	    serve_file(client,path);
-	else
-	    execute_cgi(client,path,method,query_string);
+        if ((st.st_mode & S_IFMT) == S_IFDIR)
+            strcat(path, "/index.html");
+        if ((st.st_mode & S_IXUSR) ||
+                (st.st_mode & S_IXGRP) ||
+                (st.st_mode & S_IXOTH)    )
+            cgi = 1;
+        if (!cgi)
+            serve_file(client, path);
+        else
+            execute_cgi(client, path, method, query_string);
     }
+
     close(client);
 }
 
