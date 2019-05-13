0076d817e20a980b5776e224cd029228f8d3cc89
孙猛 <meng.sun@k7.cn>
2019-05-13 17:32:02 +0800

    检查是否是headers的错误

--- main.c
+++ main.c
@@ -41,18 +41,16 @@ void cat(int client,FILE*resource)
 void headers(int client,const char *filename)
 {
     char buf[1024];
-    strcpy(buf,"HTTP/1.0 200 OK\r\n");
-    int i=send(client,buf,strlen(buf),0);
-    strcpy(buf,SERVER_STRING);
-    send(client,buf,strlen(buf),0);
-    
-    strcpy(buf,"Content-Type: text/html\r\n");
-    send(client,buf,strlen(buf),0);
-    
-    strcpy(buf,"\r\n");
-    send(client,buf,strlen(buf),0);
-    
-    int fuck=0;
+    (void)filename;  /* could use filename to determine file type */
+
+    strcpy(buf, "HTTP/1.0 200 OK\r\n");
+    send(client, buf, strlen(buf), 0);
+    strcpy(buf, SERVER_STRING);
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "Content-Type: text/html\r\n");
+    send(client, buf, strlen(buf), 0);
+    strcpy(buf, "\r\n");
+    send(client, buf, strlen(buf), 0);
 }
 void serve_file(int client,const char*filename)
 {
